770ee364f7d7f7ec8ecb82018675a85c4cce2042
Aminul Islam <aislam181043@bscse.uiu.ac.bd>
2023-04-19 14:48:53 +0600

    third commit

---
+++ Assignment-11/index.js
@@ -0,0 +1,13 @@
+
+
+function multiplicationTable(num) {
+    for (let i = 1; i <= 10; i++) {
+        let product = num * i;
+        console.log(num + " x " + i + " = " + product);
+    }
+}
+
+
+// multiplicationTable(5);
+// multiplicationTable(2);
+multiplicationTable(7);
